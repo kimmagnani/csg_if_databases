53808b3d87a4268645930201bf6b85c3f46613ed
kimmagnani <153591@csgaugustinus.nl>
2021-06-05 12:34:24 +0000

    :)

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -1,48 +1,52 @@
-<html>  
-<head>  
-    <title>PHP login system</title>   
-    <link rel = "stylesheet" type = "text/css" href = "css/design.css">   
-</head>  
-<body>  
-    <div id = "frm">  
-        <h1>Login</h1>  
-        <form name="f1" action = "php/authentication.php" onsubmit = "return validation()" method = "POST"> 
-            <p>  
-                <label> Username: </label>  
-                <input type = "text" id ="user" name  = "user" />  
-            </p>  
-            <p>  
-                <label> Password: </label>  
-                <input type = "password" id ="pass" name  = "pass" />  
-            </p>  
-            <p>     
-                <input type =  "submit" id = "btn" value = "Login" />  
-            </p>  
-        </form>  
-    </div>   
-    <script>  
-            function validation()  
-            {  
-                var id=document.f1.user.value;  
-                var ps=document.f1.pass.value;  
-                if(id.length=="" && ps.length=="") {  
-                    alert("Username and Password fields are empty");  
-                    return false;  
-                }  
-                else  
-                {  
-                    if(id.length=="") {  
-                        alert("Username is empty");  
-                        return false;  
-                    }   
-                    if (ps.length=="") {  
-                    alert("Password field is empty");  
-                    return false;  
-                    }  
-                }  
-                                           
-            }  
-    </script> 
-        <img class="image" src="images/kookhoed.jpg" alt="Hoed">
-</body>     
-</html>  
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/design.css">
+</head>
+
+<body>
+    <div id="frm">
+        <div id="plaatje">
+            <img class="image" src="images/kookhoed.jpg" alt="Hoed" class="center">
+        </div>
+        <div id="login">
+            <h1>Login</h1>
+            <form name="f1" action="php/authentication.php" onsubmit="return validation()" method="POST">
+                <p>
+                    Username:
+                    <input type="text" id="user" name="user" />
+                </p>
+                <p>
+                    <label> Password: </label>
+                    <input type="password" id="pass" name="pass" />
+                </p>
+                <p>
+                    <input type="submit" id="btn" value="Login" />
+                </p>
+            </form>
+        </div>
+    </div>
+    <script>
+        function validation() {
+            var id = document.f1.user.value;
+            var ps = document.f1.pass.value;
+            if (id.length == "" && ps.length == "") {
+                alert("Username and Password fields are empty");
+                return false;
+            } else {
+                if (id.length == "") {
+                    alert("Username is empty");
+                    return false;
+                }
+                if (ps.length == "") {
+                    alert("Password field is empty");
+                    return false;
+                }
+            }
+
+        }
+    </script>
+</body>
+
+</html>
--- public/mijnProject/php/authentication.php
+++ public/mijnProject/php/authentication.php
@@ -14,9 +14,8 @@
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
+            header("Location:homescreen.html"); 
         }  
         else{  
-            echo "<h1> Login failed. Invalid username or password.</h1>";  
-        }     
-?>  
+            echo "<h1><center> Login failed. Invalid username or password.</center></h1>";  
+        }
