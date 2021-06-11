ec5d0b5be2b87240e972da486e910c35f8576d79
kimmagnani <153591@csgaugustinus.nl>
2021-06-11 16:22:02 +0000

    hallo

---
+++ public/mijnProject/php/postmaken.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Post maken</title>
+    <link rel="stylesheet" type="text/css" href="../css/postplus.css">
+
+</head>
+
+<body>
+    <div id="blok">
+        <h1>Post maken</h1>
+    </div>
+</body>
+
+</html>
