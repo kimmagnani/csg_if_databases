35452027818d487baf207e4d2e8c953d790b85d5
kimmagnani <153591@csgaugustinus.nl>
2021-06-15 20:16:15 +0000

    Doei

--- public/mijnProject/php/postmaken.php
+++ public/mijnProject/php/postmaken.php
@@ -3,11 +3,15 @@
 
 <head>
     <title>Post maken</title>
-    <link rel="stylesheet" type="text/css" href="../css/postplus.css">
+    <link rel="stylesheet" type="text/css" href="../css/designhs.css">
 
 </head>
 
 <body>
+    <div class="myDiv">
+        <a href="homescreen.html"><img src="../images/kookhoed.png" alt="hoed"> </a>
+        <h1 class="Chalaki">Chalaki</h1>
+    </div>
     <div id="blok">
         <h1>Post maken</h1>
     </div>
