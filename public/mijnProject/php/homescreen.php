b5dd43bad02e7fd3991f803390e60eb7905b75ed
kimmagnani <153591@csgaugustinus.nl>
2021-06-25 13:53:24 +0000

    Lars is klaar

--- public/mijnProject/php/homescreen.php
+++ public/mijnProject/php/homescreen.php
@@ -18,7 +18,20 @@
         <br>
         <a class="knop2" href="postmaken.php">+</a>
     </div>
-    <div class="menu">
+    <div class="posts">
+        <h2>Dit zijn de recepten die andere mensen op de website hebben gedeeld. Wil jij ook een recept maken? Druk dan op de + knop in de bovenste balk!<br><br></h2>
+        <p><b>(De recepten staan op alfabetische volgorde zodat jij makkerlijker het recept kan vinden dat jij zoekt!)</b></p>
+        <?php
+        include('connection.php');
+        $sql = "SELECT * FROM Posts ORDER BY recept";
+        $records = mysqli_query($con, $sql);
+        if (mysqli_num_rows($records) > 0) {
+            while ($record = mysqli_fetch_assoc($records)) {
+                echo "Recept voor: " . $record["Recept"] . "<br>" . "Bereidingstijd: " . $record["Bereidingstijd"] . "<br>" . "<br>" . "Bereidingswijze: <br>" . $record["Bereidingswijze"] . "<br><br><br><br>";
+            }
+        } else {
+            echo "Nog geen posts, je kan er natuurlijk altijd zelf eentje maken :)";
+        } ?>
     </div>
 </body>
 
